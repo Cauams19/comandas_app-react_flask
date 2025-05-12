87205190b12908f5bf6c050b60fe2ded498c1327
Caua <cauamoreira27@gmail.com>
2025-05-12 15:11:44 -0300

    implementando o blueprint de clientes

--- proxy_bff/src/app.py
+++ proxy_bff/src/app.py
@@ -9,6 +9,8 @@ from funcoes import Funcoes
 
 # blueprints
 from mod_funcionario.funcionario import bp_funcionario
+from mod_cliente.cliente import bp_cliente
+#from mod_produto.produto import bp_produto
 
 # Configuração básica de logging
 logging.basicConfig(level=logging.INFO)
@@ -52,6 +54,8 @@ def before_request():
 
 # registra as rotas dos blueprints
 app.register_blueprint(bp_funcionario)
+app.register_blueprint(bp_cliente)
+#app.register_blueprint(bp_produto)
 
 # ponto de entrada para execução
 if __name__ == '__main__':
